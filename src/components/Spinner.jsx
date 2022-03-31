6ecf69c444fc090b394e9dd4dfedc8c249fb5822
Syed Ziauddin <syed.nlp00@gmail.com>
2022-03-31 11:59:00 +0530

    feat:loading animation added

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
-// import Loader from 'react-loader-spinner';
+import loading from '../assets/loading.gif';
 
 function Spinner({ message }) {
   return (
     <div className="flex flex-col justify-center items-center w-full h-full">
-      <h1>we are Adding <br/>
-        Something into <br/>
-        your feed</h1>
-
+      <img src={loading} alt="loading."/>
       <p className="text-lg text-center px-2">{message}</p>
     </div>
   );

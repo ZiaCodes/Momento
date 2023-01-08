cda4eeb47d56e1bea17426d11df275f12f2c38fd
Syed Ziauddin <syed.nlp00@gmail.com>
2023-01-08 12:44:00 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -64,26 +64,26 @@ const Login = () => {
         </div>
         </div>
 
-        <div className=" flex flex-col justify-center items-center p-2">
-          <div className="m-5 shadow-md border p-5 text-xl bg-white flex justify-between items-center text-center rounded-full">
-            <FaUserAlt/>
-            <input className='w-full px-2 border-none outline-none' type="text" placeholder='Username' />
-            </div>
+//         <div className=" flex flex-col justify-center items-center p-2">
+//           <div className="m-5 shadow-md border p-5 text-xl bg-white flex justify-between items-center text-center rounded-full">
+//             <FaUserAlt/>
+//             <input className='w-full px-2 border-none outline-none' type="text" placeholder='Username' />
+//             </div>
             
-          <div className="m-5 shadow-md border p-5 text-xl bg-white flex  justify-between items-center text-center rounded-full">
-            <AiFillLock/>
-            <input className='w-full px-2 border-none outline-none' type="password" placeholder='Password' />
-            </div>
-          <div className="m-5 shadow-md border rounded-full bg-white  text-xl w-190 text-center">
-            <button style={{
-              background:'#30BAF1',
-              color:'#fff'
-            }} className="w-full h-full p-4 px-2 rounded-full" type="submit">Login</button>
-            </div>
+//           <div className="m-5 shadow-md border p-5 text-xl bg-white flex  justify-between items-center text-center rounded-full">
+//             <AiFillLock/>
+//             <input className='w-full px-2 border-none outline-none' type="password" placeholder='Password' />
+//             </div>
+//           <div className="m-5 shadow-md border rounded-full bg-white  text-xl w-190 text-center">
+//             <button style={{
+//               background:'#30BAF1',
+//               color:'#fff'
+//             }} className="w-full h-full p-4 px-2 rounded-full" type="submit">Login</button>
+//             </div>
 
-            <p className=" text-slate-900 ">OR</p>
+//             <p className=" text-slate-900 ">OR</p>
 
-            <hr className='w-350 border-t-1 border-neutral-400 border-dashed'/>
+//             <hr className='w-350 border-t-1 border-neutral-400 border-dashed'/>
 
             <div className="flex">
 
@@ -106,17 +106,17 @@ const Login = () => {
               />
             </div>
 
-            <div style={{
-              color:'#1DA1F2',cursor:'pointer'
-            }} className="flex shadow-md border items-center m-2 p-2 text-5xl bg-white rounded-full">
-            <AiFillTwitterCircle/>
-            </div>
+//             <div style={{
+//               color:'#1DA1F2',cursor:'pointer'
+//             }} className="flex shadow-md border items-center m-2 p-2 text-5xl bg-white rounded-full">
+//             <AiFillTwitterCircle/>
+//             </div>
 
-            <div style={{
-              color:'#4267B2',cursor:'pointer'
-            }} className="flex shadow-md border items-center m-2 p-2 text-5xl bg-white rounded-full">
-            <BsFacebook/>
-            </div>
+//             <div style={{
+//               color:'#4267B2',cursor:'pointer'
+//             }} className="flex shadow-md border items-center m-2 p-2 text-5xl bg-white rounded-full">
+//             <BsFacebook/>
+//             </div>
 
             </div>
             <p className=" text-slate-900 mt-4 ">Don't have an account ?</p>
